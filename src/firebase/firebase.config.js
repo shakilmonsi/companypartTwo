c3bc37b46bf6420c5e8b35f49cf8e43a76ecde40
shakilmia420 <samdshakilofficial@gmail.com>
2023-12-24 15:57:18 +0600

    company twodd

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc5QbkCPSAPK7cQbAQuDcavxFLUMIbu8I",
+  authDomain: "bdtaskmsyproject.firebaseapp.com",
+  projectId: "bdtaskmsyproject",
+  storageBucket: "bdtaskmsyproject.appspot.com",
+  messagingSenderId: "299665703937",
+  appId: "1:299665703937:web:050c1f6cb4d761f87dac00"
+};
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
